7d79a5ea61d568a673cdd95d80905479ea62f7d0
Josue Santana <josuesantana17@gmail.com>
2021-05-10 21:55:05 -0400

    arreglando problema de disenio

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,11 @@
 <template>
 <q-page class="flex flex-center">
-    <div class="text-center q-my-xl">
-      <div class="q-px-xl">
+    <div class="full-width column wrap justify-center items-center content-center">
+      <div class="q-px-xl text-center">
         <span>“Usa tus sentidos para entender como te ves y como ves al mundo que te rodea.”</span>
       </div>
-      <div class="row justify-between">
-        <div></div>
-        <div class="q-mx-xl">
-          <span></span>
-          <span>Ajna</span>
-        </div>
+      <div style="margin-left: 265px;">
+        <span>Ajna</span>
       </div>
     </div>
     <div class="q-mt-xl-xl">
