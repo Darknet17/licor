371a7438b744384d81424b58614c4010c03fe0f5
Josue Santana <josuesantana17@gmail.com>
2021-05-12 21:49:09 -0400

    ya en la parte de inicio se hace el scrolling automatico

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
 <q-page class="flex flex-center">
-    <div class="full-width column wrap justify-center items-center content-center">
+    <div id="initiation" class="full-width column wrap justify-center items-center content-center">
       <div class="q-px-xl text-center">
         <span>“Usa tus sentidos para entender como te ves y como ves al mundo que te rodea.”</span>
       </div>
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div class="q-mt-xl-xl">
-      <ButtonSeeMore></ButtonSeeMore>
+      <ButtonSeeMore :ancle="{hash: '#fabrication-process'}" @action="anchorHashCheck"></ButtonSeeMore>
     </div>
 </q-page>
 </template>
@@ -19,7 +19,24 @@ export default {
   name: 'PageIndex',
   components: {
     ButtonSeeMore: () => import('src/components/ButtonSeeMore'),
-  }
+  },
+  methods: {
+    anchorHashCheck() {
+      try {
+        const router = `#/${this.$route.hash}`;
+        if (window.location.hash === router) {
+          document.getElementById(this.$route.hash.slice(1)).scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      }catch (e) {
+
+      }
+    },
+  },
+  mounted() {
+    this.anchorHashCheck()
+  },
 }
 </script>
 
